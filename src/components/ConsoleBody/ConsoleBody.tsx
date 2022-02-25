e466b0ec4f50a355d97dd76d02060657fbdb9489
IgorShyian <igor.szyjan.work@gmail.com>
2022-02-25 13:56:18 +0100

    finished resizable panels

--- src/components/ConsoleBody/ConsoleBody.tsx
+++ src/components/ConsoleBody/ConsoleBody.tsx
@@ -1,129 +1,57 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './consoleBody.scss';
 import { ReactComponent as FormatIcon } from '@icons/format.svg';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import Split from 'react-split';
-
-type panelsWidthType = {
-  initialPos: number;
-  initialSize: number;
-  leftPanel: number;
-  //   rightPanel: number;
-};
+import './consoleBody.scss';
 
 export const ConsoleBody = () => {
   const [loading, setLoading] = useState(false);
-  const ref = useRef(null);
-  const [panelsWidth, setPanelsWidth] = useState<panelsWidthType>({
-    initialPos: 0,
-    initialSize: 0,
-    leftPanel: 0,
-    // rightPanel: 0,
-  });
+
+  const refLeft = useRef(null);
+  const [panelsSize, setPanelsSize] = useState([50, 50]);
+
+  const [request, setRequest] = useState('');
+  const [response, setResponse] = useState('response');
+
+  const dragEnd = () => {
+    //@ts-ignore
+    const leftPanelWidth = refLeft.current.style.width.substr(5, 2);
+    const sizes = [+leftPanelWidth, +`${100 - leftPanelWidth}`];
+
+    setPanelsSize(sizes);
+
+    localStorage.setItem('panelsSize', JSON.stringify(sizes));
+  };
 
   useEffect(() => {
-    const resizeableEle = ref.current;
-
-    setPanelsWidth({
-      ...panelsWidth,
-      //@ts-ignore
-      leftPanel: resizeableEle.offsetWidth,
-      //@ts-ignore
-      //   rightPanel: resizeableEle.offsetWidth,
-    });
+    const item = localStorage.getItem('panelsSize');
+    if (item !== null) {
+      setPanelsSize(JSON.parse(item));
+    }
   }, []);
 
-  const initial = (e: any) => {
-    const resizable = document.getElementById('Resizable');
-
-    console.log(e.clientX);
-    //@ts-ignore
-    console.log(resizable.offsetWidth);
-
-    resizable &&
-      setPanelsWidth({
-        ...panelsWidth,
-        initialPos: e.clientX,
-        initialSize: resizable.offsetWidth,
-      });
-  };
-
-  const changeSize = (e: any) => {
-    const resizable = document.getElementById('Resizable');
-
-    console.log(e.clientX);
-    if (resizable && e.clientX !== 0) {
-      setPanelsWidth({
-        ...panelsWidth,
-        //@ts-ignore
-        leftPanel: panelsWidth.initialSize + (e.clientX - panelsWidth.initialPos),
-      });
-      resizable.style.width = `${panelsWidth.initialSize + (e.clientX - panelsWidth.initialPos)}px`;
-      console.log(panelsWidth.initialSize + (e.clientX - panelsWidth.initialPos));
-    }
-  };
-
-  const onTouchStart = (e: any) => {
-    const resizable = document.getElementById('Resizable');
-
-    resizable &&
-      setPanelsWidth({
-        ...panelsWidth,
-        initialPos: e.touches[0].clientX,
-        initialSize: resizable.offsetWidth,
-      });
-  };
-
-  const onTouchMove = (e: any) => {
-    const resizable = document.getElementById('Resizable');
-
-    if (resizable) {
-      resizable.style.width = `${panelsWidth.initialSize + (e.touches[0].clientX - panelsWidth.initialPos)}px`;
-
-      setPanelsWidth({
-        ...panelsWidth,
-        leftPanel: panelsWidth.initialSize + (e.touches[0].clientX - panelsWidth.initialPos),
-      });
-    }
+  const formatRequest = () => {
+    console.log(JSON.stringify(request));
   };
 
   return (
     <div className='console-body'>
-      <div className='console-body__panels'>
-        <div id='Resizable' ref={ref} style={{ width: `calc(50% - 10px)` }}>
+      <Split className='console-body__panels' minSize={400} gutterSize={15} sizes={panelsSize} onDragEnd={dragEnd}>
+        <div className='console-body__panel-block' ref={refLeft}>
           <div className='console-body__title'>Запрос:</div>
-          <div className='console-body__panel'>test1</div>
+          <div className='console-body__panel'>
+            <textarea className='console-body__textarea' value={request} onChange={e => setRequest(e.target.value)}>
+              test1
+            </textarea>
+          </div>
         </div>
-        <div
-          className='gutter'
-          draggable='true'
-          onDragStart={initial}
-          onDrag={changeSize}
-          onTouchStart={onTouchStart}
-          onTouchMove={onTouchMove}
-        ></div>
-        {/* <div className='gutter' draggable='true' onDragStart={initial} onDrag={changeSize}></div> */}
 
-        {/* <div style={{ width: `calc(${panelsWidth.leftPanel}% - 10px)` }} id='Resizable'>
-          <div className='console-body__title'>Запрос:</div>
-          <div className='console-body__panel'>test1</div>
-        </div> */}
-
-        <div style={{ height: '100%', width: `calc(100% - ${panelsWidth.leftPanel}px - 10px)` }}>
+        <div className='console-body__panel-block'>
           <div className='console-body__title'>Ответ:</div>
-          <div className='console-body__panel'>test2</div>
+          <div className='console-body__panel' style={{ padding: '10px' }}>
+            {response}
+          </div>
         </div>
-      </div>
-      {/* <Split className='console-body__panels' direction='horizontal' minSize={400} gutterSize={10}>
-        <div>
-          <div className='console-body__title'>Запрос:</div>
-          <div className='console-body__panel'>test1</div>
-        </div>
-
-        <div>
-          <div className='console-body__title'>Ответ:</div>
-          <div className='console-body__panel'>test2</div>
-        </div>
-      </Split> */}
+      </Split>
 
       <div className='row console-body__footer'>
         <div className='col-12 col-sm-4 mt-3 mt-sm-0'>
@@ -145,7 +73,7 @@ export const ConsoleBody = () => {
           </a>
         </div>
         <div className='col-12 col-sm-5 col-md-4 d-flex justify-content-sm-end mt-3 mt-sm-0'>
-          <button type='submit' className='console-body__format-button'>
+          <button type='submit' className='console-body__format-button' onClick={formatRequest}>
             <FormatIcon style={{ marginRight: '10px' }} />
             Форматировать
           </button>
